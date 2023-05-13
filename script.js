25152f4bbca5dc7361d0b8acda0c6c5dc546d6f2
j-lederer <125938397+j-lederer@users.noreply.github.com>
2023-05-12 23:10:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//alert('You successfully linked your JavaScript file!');
+
+/*
+var button = document.querySelector("button");
+	button.addEventListener("click", function() {
+	  alert("Hello World!");
+	});
+    */
+
+    var button = document.querySelector("button");
+	button.addEventListener("click", function() {
+	  alert("You submitted the following actions: ");
+	});
